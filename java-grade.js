0d1c52e6a03ca22a6c953e3607511c38f3d7b59b
Tonkruay <st2217@roong-aroon.ac.th>
2024-09-17 15:27:48 +0700

    nang

---
+++ java-grade.js
@@ -0,0 +1,35 @@
+let score = 100
+ 
+ 
+if (score < 0  || score > 100 ){
+    console.log('Error');
+
+
+} else if (score >= 80){
+console.log('A');
+
+} else if (score >= 75){
+ console.log('B+');  
+
+} else if (score >= 70){
+  console.log('B');
+
+    
+} else if (score >= 65){
+  console.log('C+');
+
+    
+} else if (score >= 60){
+   console.log('C');
+
+    
+} else if (score >= 55){
+    console.log('D+');
+    
+} else if (score >= 50){
+    console.log('D');
+    
+}else if (score >= 0){
+    console.log('F');
+    
+}
